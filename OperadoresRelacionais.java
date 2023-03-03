05404dd67d7042658d0f98fb6f85bd6dfce0d5a2
joaodedeusrsfilho <joaodedeusrsfilho@gmail.com>
2023-03-03 10:07:43 -0300

    add file

---
+++ OperadoresRelacionais.java
@@ -0,0 +1,19 @@
+package com.junior.curso_java_basico.aula13;
+
+public class OperadoresRelacionais {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int valor1 = 1;
+		int valor2 = 2;
+		
+		System.out.println("Valor1 == Valor2 = " +(valor1==valor2));
+		System.out.println("Valor1 != Valor2 = " +(valor1!=valor2));
+		System.out.println("Valor1 > Valor2 = " +(valor1>valor2));
+		System.out.println("Valor1 >= Valor2 = " +(valor1>=valor2));
+		System.out.println("Valor1 < Valor2 = " +(valor1<valor2));
+		System.out.println("Valor1 <= Valor2 = " +(valor1<=valor2));
+
+	}
+
+}
